1b7ee0198b8d2273d03e98c5e239429c4f76ea90
howardchan-oocl <74343984+howardchan-oocl@users.noreply.github.com>
2020-11-15 17:00:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,59 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
-
+console.log(typeof a); //No, it is a string
+console.log(typeof b); //No, it is an object
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
-
+for (var i = 0; i < a.length; i++) {
+  a[i] = 2*a[i];
+}
+console.log(a);
 
 // Write the program, according to the following requirements output results.
 var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
-
+var case1 = colors[0];
+var case2 = colors[0];
+var case3 = colors[0];
+for (var i = 1; i < colors.length; i++) {
+  case1 = case1 + " " + colors[i];
+  case2 = case2 + "+" + colors[i];
+  case3 = case3 + "," + colors[i];
+}
+console.log(case1+" "+case2+" "+case3)
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
-
+a.sort(function(a, b) {
+  return b - a;
+});
+console.log(a)
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+a.sort();
+var result;
+var result_count=0;
+var element;
+var count=0;
+for (var i = 0; i < a.length; i++) {
+  if(element===a[i]){
+      count = count + 1;
+  }else{
+    element=a[i];
+    count=1;
+  }
+  if(result_count<count){
+    result_count=count;
+    result=element;
+  }
+}
+console.log(result);
+console.log(result_count);
